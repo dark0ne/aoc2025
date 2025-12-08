584beea56bf9cf09436a07de0d74e9ccc09f016a
Ivan Begert <Ivan.Begert@vector.com>
2025-12-08 11:48:28 +0100

    day 8 using disjoint-set data structure.

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -1,7 +1,5 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-};
+use disjoint::DisjointSet;
+use std::{collections::HashSet, fs};
 fn main() {
     let input = fs::read_to_string("in1.txt").unwrap();
     let result = step1(&input, 1000);
@@ -13,7 +11,6 @@ fn main() {
 
 #[derive(Clone, Copy, Hash)]
 struct P {
-    id: usize,
     x: usize,
     y: usize,
     z: usize,
@@ -28,223 +25,71 @@ fn distance_sqr(p1: &P, p2: &P) -> f32 {
 
 fn step1(input: &str, limit: usize) -> usize {
     let lines: Vec<&str> = input.split('\n').filter(|l| !l.is_empty()).collect();
-    let mut next_id = 0;
 
     let mut points = Vec::new();
     for l in lines.iter() {
         let nums: Vec<usize> = l.split(',').map(|n| n.parse().unwrap()).collect();
-        let p = P {
-            id: next_id,
+        points.push(P {
             x: nums[0],
             y: nums[1],
             z: nums[2],
-        };
-        next_id += 1;
-
-        points.push(p);
+        });
     }
 
     let mut distances = Vec::new();
-    for (a, p1) in points.iter().enumerate() {
-        for p2 in points.iter().skip(a + 1) {
+    for (p1_idx, p1) in points.iter().enumerate() {
+        for (p2_idx, p2) in points.iter().enumerate().skip(p1_idx + 1) {
             let dist = distance_sqr(p1, p2);
-            distances.push((p1.id, p2.id, dist));
+            distances.push((p1_idx, p2_idx, dist));
         }
     }
 
     distances.sort_by(|d1, d2| f32::total_cmp(&d1.2, &d2.2));
 
-    let mut all_circuits: HashMap<usize, CircuitList> = HashMap::new();
-    let mut point_to_circuit_size: HashMap<usize, usize> = HashMap::new();
+    let mut all_circuits = DisjointSet::with_len(points.len());
 
-    let mut single_list = CircuitList::new();
-    for p in points.iter() {
-        let mut single_circuit = Circuit::new();
-        single_circuit.add_point(p.id);
-        single_list.list.push(single_circuit);
-
-        point_to_circuit_size.insert(p.id, 1);
+    for dist in distances.iter().take(limit) {
+        let _ = all_circuits.join(dist.0, dist.1);
     }
 
-    all_circuits.insert(1, single_list);
+    let sizes: HashSet<usize> = all_circuits.sets().into_iter().map(|v| v.len()).collect();
 
-    'skip: for dist in distances.iter().take(limit) {
-        let p1_circ_size = point_to_circuit_size[&dist.0];
-        let p2_circ_size = point_to_circuit_size[&dist.1];
-
-        // Part of the same circuit?
-        if p1_circ_size == p2_circ_size {
-            for circuit in all_circuits[&p1_circ_size].list.iter() {
-                if circuit.is_point_in(dist.0) && circuit.is_point_in(dist.1) {
-                    continue 'skip;
-                }
-            }
-        }
-
-        let mut c1 = all_circuits
-            .get_mut(&p1_circ_size)
-            .unwrap()
-            .remove_circuit_with_point(dist.0);
-
-        let c2 = all_circuits
-            .get_mut(&p2_circ_size)
-            .unwrap()
-            .remove_circuit_with_point(dist.1);
-
-        all_circuits.retain(|_, list| !list.list.is_empty());
-
-        c2.points.iter().for_each(|p| {
-            c1.points.insert(*p);
-        });
-        let new_size = p1_circ_size + p2_circ_size;
-
-        c1.points
-            .iter()
-            .for_each(|p| *point_to_circuit_size.get_mut(p).unwrap() = new_size);
-
-        let entry = all_circuits.entry(new_size).or_insert(CircuitList::new());
-        entry.list.push(c1);
-
-        // dbg!(&all_circuits);
-    }
-
-    let mut keys = all_circuits
-        .iter()
-        .filter(|(_, c)| !c.list.is_empty())
-        .map(|(k, _)| *k)
-        .collect::<Vec<usize>>();
+    let mut keys: Vec<usize> = sizes.into_iter().collect();
     keys.sort();
     keys.reverse();
-    dbg!(&keys);
     keys.iter().take(3).product()
-}
-
-#[derive(Debug)]
-struct Circuit {
-    points: HashSet<usize>,
-}
-
-impl Circuit {
-    fn new() -> Self {
-        Self {
-            points: HashSet::new(),
-        }
-    }
-    fn is_point_in(&self, p: usize) -> bool {
-        self.points.contains(&p)
-    }
-    fn add_point(&mut self, p: usize) {
-        self.points.insert(p);
-    }
-}
-
-#[derive(Debug)]
-struct CircuitList {
-    list: Vec<Circuit>,
-}
-
-impl CircuitList {
-    fn new() -> Self {
-        Self { list: Vec::new() }
-    }
-    fn remove_circuit_with_point(&mut self, p: usize) -> Circuit {
-        if let Some((idx, _)) = self.list.iter().enumerate().find(|(_, c)| c.is_point_in(p)) {
-            return self.list.remove(idx);
-        }
-        panic!("Not found {p}");
-    }
 }
 
 fn step2(input: &str) -> usize {
     let lines: Vec<&str> = input.split('\n').filter(|l| !l.is_empty()).collect();
-    let mut next_id = 0;
-
     let mut points = Vec::new();
     for l in lines.iter() {
         let nums: Vec<usize> = l.split(',').map(|n| n.parse().unwrap()).collect();
-        let p = P {
-            id: next_id,
+        points.push(P {
             x: nums[0],
             y: nums[1],
             z: nums[2],
-        };
-        next_id += 1;
-
-        points.push(p);
+        });
     }
 
     let mut distances = Vec::new();
-    for (a, p1) in points.iter().enumerate() {
-        for p2 in points.iter().skip(a + 1) {
+    for (p1_idx, p1) in points.iter().enumerate() {
+        for (p2_idx, p2) in points.iter().enumerate().skip(p1_idx + 1) {
             let dist = distance_sqr(p1, p2);
-            distances.push((p1.id, p2.id, dist));
+            distances.push((p1_idx, p2_idx, dist));
         }
     }
 
     distances.sort_by(|d1, d2| f32::total_cmp(&d1.2, &d2.2));
 
-    let mut all_circuits: HashMap<usize, CircuitList> = HashMap::new();
-    let mut point_to_circuit_size: HashMap<usize, usize> = HashMap::new();
+    let mut all_circuits = DisjointSet::with_len(points.len());
 
-    let mut single_list = CircuitList::new();
-    for p in points.iter() {
-        let mut single_circuit = Circuit::new();
-        single_circuit.add_point(p.id);
-        single_list.list.push(single_circuit);
-
-        point_to_circuit_size.insert(p.id, 1);
-    }
-
-    all_circuits.insert(1, single_list);
-
-    'skip: for dist in distances.iter() {
-        let p1_circ_size = point_to_circuit_size[&dist.0];
-        let p2_circ_size = point_to_circuit_size[&dist.1];
-
-        // Part of the same circuit?
-        if p1_circ_size == p2_circ_size {
-            for circuit in all_circuits[&p1_circ_size].list.iter() {
-                if circuit.is_point_in(dist.0) && circuit.is_point_in(dist.1) {
-                    continue 'skip;
-                }
+    for dist in distances.iter() {
+        if all_circuits.join(dist.0, dist.1) {
+            if all_circuits.sets().len() == 1 {
+                return points[dist.0].x * points[dist.1].x;
             }
         }
-
-        let mut c1 = all_circuits
-            .get_mut(&p1_circ_size)
-            .unwrap()
-            .remove_circuit_with_point(dist.0);
-
-        let c2 = all_circuits
-            .get_mut(&p2_circ_size)
-            .unwrap()
-            .remove_circuit_with_point(dist.1);
-
-        all_circuits.retain(|_, list| !list.list.is_empty());
-
-        c2.points.iter().for_each(|p| {
-            c1.points.insert(*p);
-        });
-        let new_size = p1_circ_size + p2_circ_size;
-
-        c1.points
-            .iter()
-            .for_each(|p| *point_to_circuit_size.get_mut(p).unwrap() = new_size);
-
-        let entry = all_circuits.entry(new_size).or_insert(CircuitList::new());
-        entry.list.push(c1);
-
-        if all_circuits
-            .iter()
-            .map(|(_, list)| list.list.len())
-            .sum::<usize>()
-            == 1
-        {
-            // we are done
-            return points[dist.0].x * points[dist.1].x;
-        }
-
-        // dbg!(&all_circuits);
     }
 
     panic!("Should not happen");
